9c06fb54454202089c44542cdbcd8ee834cce288
nomori <nomore.zm@gmail.com>
2020-10-12 02:19:02 +0900

    Chapter01.
    print関係.

---
+++ Capture/Cap1/chap1.py
@@ -0,0 +1,16 @@
+abc = "abcdefghijklmn"
+
+# 変数を出力
+print(abc)
+
+# index 一致する文字列の位置を返却する.
+print(abc.index("h"))
+
+# 文字列を全て大文字に変換する.
+# lower <-> upper
+print(abc.upper())
+
+# 文字列を置換する.
+# 一致する文字列を置換する
+print(abc.replace('abc', '123'))
+
