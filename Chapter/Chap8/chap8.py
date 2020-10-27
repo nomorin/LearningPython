3ff5c3986b8a6afc3d41d629398bafbccb193e00
nomori <nomore.zm@gmail.com>
2020-10-28 06:10:40 +0900

    [add]Decimalの使い方.

--- Chapter/Chap8/chap8.py
+++ Chapter/Chap8/chap8.py
@@ -1,7 +1,24 @@
-lang_list = ['Java', 'perl', 'PHP', 'Python', 'Ruby']
+from decimal import *
 
-print(lang_list)
+# lang_list = ['Java', 'perl', 'PHP', 'Python', 'Ruby']
+#
+# print(lang_list)
+#
+# lang_list.remove('Java')
+# print(lang_list)
 
-lang_list.remove('Java')
-print(lang_list)
+d = Decimal(10)
+print(d)
+
+# 整数の加算.
+print(d + 20)
+
+# 小数の加算.
+# 小数の場合は文字列をDecimalに渡す
+print(d + Decimal("0.2"))
+
+# 平方根の計算
+print(d.sqrt())
+
+
 
